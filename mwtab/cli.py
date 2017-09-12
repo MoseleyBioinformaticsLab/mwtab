1facc38b2231e181f105654847ca38e024e993f7
Andrey Smelter <smelandr@gmail.com>
2017-09-11 22:30:25 -0400

    CLI update.

--- mwtab/cli.py
+++ mwtab/cli.py
@@ -8,16 +8,17 @@ The mwtab command-line interface
 Usage:
     mwtab -h | --help
     mwtab --version
-    mwtab convert (<from_path> <to_path>) [--from_format=<format>] [--to_format=<format>] [--validate] [--verbose]
-    mwtab validate <from_path> [--verbose]
+    mwtab convert (<from-path> <to-path>) [--from-format=<format>] [--to-format=<format>] [--validate] [--verbose]
+    mwtab validate <from-path> [--verbose]
     
 Options:
     -h, --help                      Show this screen.
     --version                       Show version.
     --verbose                       Print what files are processing.
     --validate                      Validate the mwTab file.
-    --from_format=<format>          Input file format, available formats: mwtab, json [default: mwtab].
-    --to_format=<format>            Output file format, available formats: mwtab, json [default: json].
+    --from-format=<format>          Input file format, available formats: mwtab, json [default: mwtab].
+    --to-format=<format>            Output file format, available formats: mwtab, json [default: json].
+    --mw-rest=<url>                 URL to MW REST interface [default: "http://www.metabolomicsworkbench.org/rest/study/analysis_id/{}/mwtab/txt"].
 """
 
 import mwtab
@@ -28,15 +29,18 @@ from mwtab.mwschema import section_schema_mapping
 
 def cli(cmdargs):
 
+    mwtab.fileio.VERBOSE = cmdargs["--verbose"]
+    mwtab.fileio.MWREST = cmdargs["--verbose"]
+
     if cmdargs["convert"]:
-        converter = Converter(from_path=cmdargs["<from_path>"],
-                              to_path=cmdargs["<to_path>"],
-                              from_format=cmdargs["--from_format"],
-                              to_format=cmdargs["--to_format"])
+        converter = Converter(from_path=cmdargs["<from-path>"],
+                              to_path=cmdargs["<to-path>"],
+                              from_format=cmdargs["--from-format"],
+                              to_format=cmdargs["--to-format"])
         converter.convert()
 
     elif cmdargs["validate"]:
-        for mwfile in mwtab.read_files(cmdargs["<from_path>"]):
+        for mwfile in mwtab.read_files(cmdargs["<from-path>"]):
             validate_file(mwtabfile=mwfile,
                           section_schema_mapping=section_schema_mapping,
                           validate_samples=True,
