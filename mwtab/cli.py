f52c6c47537e8b07a86225574597a4d27bde41ea
Andrey Smelter <smelandr@gmail.com>
2017-09-14 14:00:38 -0400

    Fixed incorrect MW REST interface URL address.

--- mwtab/cli.py
+++ mwtab/cli.py
@@ -8,8 +8,8 @@ The mwtab command-line interface
 Usage:
     mwtab -h | --help
     mwtab --version
-    mwtab convert (<from-path> <to-path>) [--from-format=<format>] [--to-format=<format>] [--validate] [--verbose]
-    mwtab validate <from-path> [--verbose]
+    mwtab convert (<from-path> <to-path>) [--from-format=<format>] [--to-format=<format>] [--validate] [--mw-rest=<url>] [--verbose]
+    mwtab validate <from-path> [--mw-rest=<url>] [--verbose]
     
 Options:
     -h, --help                      Show this screen.
@@ -18,7 +18,7 @@ Options:
     --validate                      Validate the mwTab file.
     --from-format=<format>          Input file format, available formats: mwtab, json [default: mwtab].
     --to-format=<format>            Output file format, available formats: mwtab, json [default: json].
-    --mw-rest=<url>                 URL to MW REST interface [default: "http://www.metabolomicsworkbench.org/rest/study/analysis_id/{}/mwtab/txt"].
+    --mw-rest=<url>                 URL to MW REST interface [default: http://www.metabolomicsworkbench.org/rest/study/analysis_id/{}/mwtab/txt].
 """
 
 import mwtab
@@ -30,7 +30,7 @@ from mwtab.mwschema import section_schema_mapping
 def cli(cmdargs):
 
     mwtab.fileio.VERBOSE = cmdargs["--verbose"]
-    mwtab.fileio.MWREST = cmdargs["--verbose"]
+    mwtab.fileio.MWREST = cmdargs["--mw-rest"]
 
     if cmdargs["convert"]:
         converter = Converter(from_path=cmdargs["<from-path>"],
@@ -41,6 +41,8 @@ def cli(cmdargs):
 
     elif cmdargs["validate"]:
         for mwfile in mwtab.read_files(cmdargs["<from-path>"]):
+            print("mwfile:", mwfile)
+
             validate_file(mwtabfile=mwfile,
                           section_schema_mapping=section_schema_mapping,
                           validate_samples=True,
