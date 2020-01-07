12544616ae168360d22690e4c9a37c7bdafac1ce
Christian <cdpo224@g.uky.edu>
2020-01-06 21:17:47 -0500

    Adds basic outline to script for testing data files to see if useable data is present in the file (not to be included in public release).

--- tests/test_useless_files.py
+++ tests/test_useless_files.py
@@ -1,1 +1,22 @@
 # TODO: This file should not be included in the public release
+import mwtab
+from os import walk
+
+processing_errors = [
+    "AN000404", "AN000405", "AN000410", "AN000415", "AN000436", "AN000439", "AN001311", "AN001312", "AN001313",
+    "AN000696", "AN001467", "AN001576", "AN001684", "AN001685", "AN001721", "AN001979", "AN002035", "AN001492",
+    "AN001493", "AN001499", "AN001761", "AN001762", "AN001776", "AN001777", "AN001982", "AN001689", "AN001690",
+    "AN001992"
+]
+
+if __name__ == '__main__':
+
+    has_mnr_data = list()
+    has_ms_data = list()
+    has_raw_file = list()
+
+    (_, _, filenames) = next(walk("/mlab/data/cdpo224/mwtab/data"))
+    filenames = sorted(filenames)
+    for filename in filenames:
+        if not any(error in filename for error in processing_errors):
+            mwfile = next(mwtab.read_files("/mlab/data/cdpo224/mwtab/data/{}".format(filename)))
