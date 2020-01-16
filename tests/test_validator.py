393ab901cf45cc60b769c8dbefcef691dafc9e80
Christian <cdpo224@g.uky.edu>
2020-01-16 09:44:05 -0500

    Continues testing improvements to validator.py methods.

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -230,13 +230,10 @@ if __name__ == '__main__':
                 del unique_fields[k]
                 break
 
-    print(len(unique_fields))
     features = sorted(unique_fields.items(), key=lambda x: x[0].lower(), reverse=True)
-    print(features)
-
     max_len = max(len(f[0]) for f in features)
     for f, n in features:
-        print("{feature :<{width}} {num}".format(feature=f, num=n, width=max_len))
+        print("{{f}:<{w}} {num}".format(f=f, num=n, w=max_len))
 
     print()
     for k in duplicate_fields.keys():
