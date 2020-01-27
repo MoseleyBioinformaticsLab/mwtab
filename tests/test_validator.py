0a64d2d59781f3351b14da14aed6cde1042ad9cc
Christian <cdpo224@g.uky.edu>
2020-01-27 10:22:10 -0500

    Updates methods in test_validator.py to allow for testing across Python versions.

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -14,18 +14,24 @@ def test_validate(files_source):
 
 def test_validate_ms_samples():
     mwfile = next(mwtab.read_files("example_data/validation_files/AN000001_error_1.txt"))
-    validation_errors = mwtab.validate_file(mwfile, validate_factors=False, validate_features=False, validate_schema=False)
+    validation_errors = mwtab.validate_file(mwfile, validate_factors=False, validate_features=False,
+                                            validate_schema=False)
     assert len(validation_errors) == 1
-    assert repr(validation_errors[0]) == "KeyError('Missing key `Samples` in `MS_METABOLITE_DATA` block.')"
+    test_error = KeyError("Missing key `Samples` in `MS_METABOLITE_DATA` block.")
+    assert type(validation_errors[0]) == type(test_error) and validation_errors[0].args == test_error.args
 
     mwfile = next(mwtab.read_files("example_data/validation_files/AN000001_error_2.txt"))
-    validation_errors = mwtab.validate_file(mwfile, validate_factors=False, validate_features=False, validate_schema=False)
+    validation_errors = mwtab.validate_file(mwfile, validate_factors=False, validate_features=False,
+                                            validate_schema=False)
     assert len(validation_errors) == 3
-    assert repr(validation_errors[0]) == "ValueError('Sample with no Sample ID (\"\") in `SUBJECT_SAMPLE_FACTOR` block.')"
-    assert repr(validation_errors[1]) == "ValueError('Sample with no Sample ID (\"\") in `MS_METABOLITE_DATA` block " \
-                                         "(usually caused by extraneous TAB at the end of line).')"
-    # TODO: Attempt to cleanup the following line.
-    assert repr(validation_errors[2]).replace("\"", "'").replace("\\n", "").replace("\\t", "") == "ValueError('`MS_METABOLITE_DATA` block contains additional samples not found in `SUBJECT_SAMPLE_FACTORS` block.Additional samples: {'LabF_115873'}')"
+    test_error = ValueError("Sample with no Sample ID (\"\") in `SUBJECT_SAMPLE_FACTOR` block.")
+    assert type(validation_errors[0]) == type(test_error) and validation_errors[0].args == test_error.args
+    test_error = ValueError("Sample with no Sample ID (\"\") in `MS_METABOLITE_DATA` block (usually caused by "
+                            "extraneous TAB at the end of line).")
+    assert type(validation_errors[1]) == type(test_error) and validation_errors[1].args == test_error.args
+    test_error = ValueError("`MS_METABOLITE_DATA` block contains additional samples not found in "
+                            "`SUBJECT_SAMPLE_FACTORS` block.\n\tAdditional samples: {'LabF_115873'}")
+    assert type(validation_errors[2]) == type(test_error) and validation_errors[2].args == test_error.args
 
 
 def test_validate_nmr_samples():
@@ -33,16 +39,21 @@ def test_validate_nmr_samples():
     validation_errors = mwtab.validate_file(mwfile, validate_factors=False, validate_features=False,
                                             validate_schema=False)
     assert len(validation_errors) == 1
-    assert repr(validation_errors[0]) == "KeyError('Missing key `Bin range(ppm)` in `NMR_BINNED_DATA` block.')"
+    test_error = KeyError("Missing key `Bin range(ppm)` in `NMR_BINNED_DATA` block.")
+    assert type(validation_errors[0]) == type(test_error) and validation_errors[0].args == test_error.args
 
     mwfile = next(mwtab.read_files("example_data/validation_files/AN000041_error_2.txt"))
-    validation_errors = mwtab.validate_file(mwfile, validate_factors=False, validate_features=False, validate_schema=False)
+    validation_errors = mwtab.validate_file(mwfile, validate_factors=False, validate_features=False,
+                                            validate_schema=False)
     assert len(validation_errors) == 3
-    assert repr(validation_errors[0]) == "ValueError('Sample with no Sample ID (\"\") in `SUBJECT_SAMPLE_FACTOR` block.')"
-    assert repr(validation_errors[1]) == "ValueError('Sample with no sample ID (\"\") in `NMR_BINNED_DATA` block " \
-                                         "(usually caused by extraneous TAB at the end of line).')"
-    # TODO: Attempt to cleanup the following line.
-    assert repr(validation_errors[2]).replace("\"", "'").replace("\\n", "").replace("\\t", "") == "ValueError('`NMR_BINNED_DATA` block contains additional samples not found in `SUBJECT_SAMPLE_FACTORS` block.Additional samples: {'C0559'}')"
+    test_error = ValueError("Sample with no Sample ID (\"\") in `SUBJECT_SAMPLE_FACTOR` block.")
+    assert type(validation_errors[0]) == type(test_error) and validation_errors[0].args == test_error.args
+    test_error = ValueError("Sample with no sample ID (\"\") in `NMR_BINNED_DATA` block (usually caused by extraneous "
+                            "TAB at the end of line).")
+    assert type(validation_errors[1]) == type(test_error) and validation_errors[1].args == test_error.args
+    test_error = ValueError("`NMR_BINNED_DATA` block contains additional samples not found in `SUBJECT_SAMPLE_FACTORS` "
+                            "block.\n\tAdditional samples: {'C0559'}")
+    assert type(validation_errors[2]) == type(test_error) and validation_errors[2].args == test_error.args
 
 
 def test_validate_factors():
